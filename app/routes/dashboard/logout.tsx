244dfa770328bd31baf8fcddbd1d78bc1472a6a9
Alex <david_alejandrorizo@outlook.com>
2025-08-29 15:40:23 -0600

    git push

--- app/routes/dashboard/logout.tsx
+++ app/routes/dashboard/logout.tsx
@@ -1,17 +1,11 @@
 import { Navigate, redirect } from "react-router";
 import { logout } from "~/services/auth-service";
-import type { Route } from "./+types/logout";
 import { queryClient } from "~/services/queryClient";
 
 export async function clientAction() {
   await logout();
   queryClient.clear();
-  return redirect("/auth", {
-    headers: {
-      "Set-Cookie":
-        "access_token=; Max-Age=0; Path=/; Domain=.alowee.com; HttpOnly; Secure; SameSite=Lax",
-    },
-  });
+  return redirect("/auth");
 }
 
 const Logout = () => <Navigate to="/auth" />;
