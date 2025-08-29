08d039f6dbd4d709a91cb2cca113e253888d5345
Alex <david_alejandrorizo@outlook.com>
2025-08-29 14:15:01 -0600

    git push

--- app/routes/auth/_layout.tsx
+++ app/routes/auth/_layout.tsx
@@ -6,6 +6,8 @@ import { getCookie } from '~/lib/cookies';
 export async function loader({ request }: Route.LoaderArgs) {
   const authStatus = await checkAuth(getCookie(request));
 
+  console.log({authStatus, request: request.headers});
+  
   if (authStatus.user) {
     return redirect("/");
   }
