839bd869c55264ec7f4b2a85a1a4770fa454311a
Alex <david_alejandrorizo@outlook.com>
2025-08-29 14:31:09 -0600

    test cookie

--- app/routes/auth/_layout.tsx
+++ app/routes/auth/_layout.tsx
@@ -1,30 +1,33 @@
-import { Outlet, redirect } from 'react-router'
-import type { Route } from './+types/_layout';
-import { checkAuth } from '~/services/auth-service';
-import { getCookie } from '~/lib/cookies';
-import { useEffect } from 'react';
+import { Outlet, redirect } from "react-router";
+import type { Route } from "./+types/_layout";
+import { checkAuth } from "~/services/auth-service";
+import { getCookie } from "~/lib/cookies";
+import { useEffect } from "react";
+
+import { parse } from "cookie";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const authStatus = await checkAuth(getCookie(request));
 
-  const requestHeaders = request.headers;
+  const cookie = request.headers.get("cookie");
+  const requestHeaders = parse(request.headers.get("cookie") || "");
 
   if (authStatus.user) {
     return redirect("/");
   }
 
-  return { authStatus, requestHeaders };
+  return { authStatus, cookie, requestHeaders };
 }
 
 const AuthLayout = ({ loaderData }: Route.ComponentProps) => {
   useEffect(() => {
-    console.log({loaderData});
+    console.log({ loaderData });
   }, [loaderData]);
   return (
-    <main className='h-screen w-screen'>
+    <main className="h-screen w-screen">
       <Outlet />
     </main>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
