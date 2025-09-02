da2c275285fa0c92604b4346a9b899e4dabec29a
Alex <david_alejandrorizo@outlook.com>
2025-09-02 11:45:41 -0600

    test process and import.meta

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,4 +1,4 @@
 export const clientEnv = {
-  API_URL: import.meta.env.VITE_API_URL,
-  SOCKET_URL: import.meta.env.VITE_SOCKET_URL,
+  API_URL: import.meta.env.VITE_API_URL || process.env.VITE_API_URL,
+  SOCKET_URL: import.meta.env.VITE_SOCKET_URL || process.env.VITE_SOCKET_URL,
 }
